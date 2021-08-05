99becc1be67fc0d329777079e696ce311c96f719
asmaatalha <asmaa.talha1998@gmail.Com>
2021-08-05 10:26:28 +0100

    view page step

--- app/Views/UsersView/step.php
+++ app/Views/UsersView/step.php
@@ -1,1 +1,14 @@
 <?php echo "Hello"; ?>
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<div class="">
+    <form class="" action="" method="post">
+
+        <label for="desc">Description: </label>
+        <textarea type="text" name="descS" ></textarea>
+
+        <input type="submit" name="submit" value="Submit">
+
+    </form>
+
+</div>
