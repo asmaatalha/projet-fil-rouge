ad3f28d7e52f292bf03236fcb26fa6d4d068b28f
asmaatalha <asmaa.talha1998@gmail.Com>
2021-08-22 21:06:42 +0100

    front end page insert

--- app/Views/UsersView/insert.php
+++ app/Views/UsersView/insert.php
@@ -1,24 +1,45 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
-<div class="">
-    <form class="insertR-form" action="" method="post" enctype="multipart/form-data">
-        <label for="title">Title: </label>
-        <input type="text" name="Title" placeholder="title">
+<div class="cart_insert">
+    <div class="d-flex add1">
+        <img src="../icon/notebook.png" alt="add" width="5%" height="60%">
+        <h1>Add Recite</h1>
+    </div>
 
-        <label for="desc">Description: </label>
-        <textarea type="text" name="Descp" ></textarea>
+    <div class="form_insert">
 
-        <label for="img">Img: </label>
-        <input type="file" name="Img">
+        <form class="insertR" action="" method="post" enctype="multipart/form-data">
 
-        <label for="appt">Select a time:</label>
-        <input type="time" id="appt" name="time">
+            <div>
+                <label for="title">Title: </label>
+                <input type="text" name="Title" placeholder="title">
+            </div>
+            
+            <div>
+                <label for="desc">Description: </label>
+                <textarea type="text" name="Descp" ></textarea>
+            </div>
 
-        <label for="nomp">Nompre de stape: </label>
-        <input type="number" name="nStep" placeholder="numpTime">
+            <div>
+                <label for="img">Img: </label>
+                <input type="file" name="Img">
+            </div>
 
-        <input type="submit" name="submit" value="Submit">
+            <div>
+                <label for="appt">Select a time:</label>
+                <input type="time" id="appt" name="time">
+            </div>
 
-    </form>
+            <div>
+                <label for="nomp">Nompre de stape: </label>
+                <input type="number" name="nStep" placeholder="numpTime">
+            </div>
+
+            <div>
+                <input type="submit" name="submit" value="Add">
+            </div>
+            
+        </form>
+    </div>
 
 </div>
